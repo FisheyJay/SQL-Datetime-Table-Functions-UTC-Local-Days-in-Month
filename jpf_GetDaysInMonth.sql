24841e0fda28ce57ad974d6ec0d082986e1ee0d4
John Fisher <fisher.johnp@gmail.com>
2018-07-11 22:34:22 -0400

    Update jpf_GetDaysInMonth.sql

--- jpf_GetDaysInMonth.sql
+++ jpf_GetDaysInMonth.sql
@@ -1,28 +1,27 @@
 /*
  John Fisher
-
-	Create a Scalar-valued Function that will return the number of days in the month of interest, 
-	but first, check for the existence of existing functions for the following OBJECT_ID and type
-
+ 07-11-2018
+	Create a useful little Scalar-valued Function that will return the number of days in the month of interest, 
+	but first, check for the existence of this function regardless of type. That is, make sure we'll not overwrite 
+	any existing function having this same name.
 	FN = SQL scalar function
 	IF = SQL inline table-valued function
 	TF = SQL table-valued-function
 	FS = Assembly (CLR) scalar-function
 	FT = Assembly (CLR) table-valued function
-
 */
 USE [FisheyJay]
 GO
 
-/****** Object:  UserDefinedFunction [dbo].[cmr_GetDaysInMonth]    Script Date: 07/11/2018 22:12:37 ******/
-IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[cmr_GetDaysInMonth]') AND type in (N'FN', N'IF', N'TF', N'FS', N'FT'))
+/****** Object:  UserDefinedFunction [dbo].[jpf_GetDaysInMonth]    Script Date: 07/11/2018 22:12:37 ******/
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[jpf_GetDaysInMonth]') AND type in (N'FN', N'IF', N'TF', N'FS', N'FT'))
 DROP FUNCTION [dbo].[jpf_GetDaysInMonth]
 GO
 
 USE [FisheyJay]
 GO
 
-/****** Object:  UserDefinedFunction [dbo].[cmr_GetDaysInMonth]    Script Date: 07/11/2018 22:12:37 ******/
+/****** Object:  UserDefinedFunction [dbo].[jpf_GetDaysInMonth]    Script Date: 07/11/2018 22:12:37 ******/
 SET ANSI_NULLS ON
 GO
 
